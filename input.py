87b72f75b656a1144c7c9d9e3762c10088d5f90b
David <david-kok@hotmail.com>
2016-11-07 23:19:11 +0100

    tekens restrictie toegevoegd en popup na submit

--- input.py
+++ input.py
@@ -1,4 +1,6 @@
+import tkinter as tk
 from tkinter import *
+from tkinter import ttk
 from pathlib import Path
 
 file = Path('tweetQue.txt')
@@ -19,16 +21,29 @@ class loginFrame(Frame):
 
     def submit(self):
         text = Frame.input_textbox.get('1.0',END)
+        if len(text) < 4:
+            popMessage("Tweet moet minimaal 4 tekens lang zijn!.")
+        elif len(text) < 140:
+            if not file.is_file() :
+                tweetFile = open('TweetQue.txt', 'w')
+            else:
+                tweetFile = open('TweetQue.txt', 'a')
 
-        if not file.is_file() :
-            tweetFile = open('TweetQue.txt', 'w')
+            tweetFile.write(text)
+            popMessage("Tweet is verzonden en wacht op goedkeuring.")
+            tweetFile.close()
         else:
-            tweetFile = open('TweetQue.txt', 'a')
-
-        tweetFile.write(text)
-        tweetFile.close()
+            popMessage("Tweet mag maximaal 140 karakters bevatten!.")
 
 
+def popMessage(msg):
+    popup = tk.Tk()
+    popup.wm_title("Tweet info")
+    label = ttk.Label(popup, text=msg)
+    label.pack()
+    button = ttk.Button(popup, text="Ok!", command = popup.destroy)
+    button.pack()
+    popup.mainloop()
 
 lfTk = Tk()
 lfTk.title('NS Tweetbot')
