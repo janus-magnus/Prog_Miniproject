4667a8bfd502aae13e0e5f6fdddc463a84ddcd57
janus-magnus <Ykorringa@gmail.com>
2016-11-09 15:44:16 +0100

    weather

--- API_controler.py
+++ API_controler.py
@@ -1,6 +1,6 @@
 from twython import Twython
 from pyowm import *
-from django.core.cache import cache
+
 
 # Twittwer API Stuff
 T_API_key = 'r44TpHBZ5ycKGiaSKDGkcBDEP'  # Twitter App API key
@@ -8,19 +8,25 @@ T_API_key_secret = 'r8oMp8hPdtZsezXKYvwgH4Xf5XD2MneflJzpJfiBsQAz9rQNXc' # secret
 T_OAuth_TK = '795684707501539330-B5vblQn2f2ELJLeQxcRm1Ta8bdCb45e'  # O Auth token
 T_OAuth_TK_secret = 'Uw6ePHCuttGXvFP1FPHqeTTg4cC3JDRP1POUlqZaWu2Hn'  # O auth token secret moet eigenlijk niet leesbaar zijn hier
 # weather API
-W_API_key = '23cf4ba4eba9811dec72a34c4a61b422'  # App API key
+W_API_key = '0292ef38878076e5a546762d1c2a5ff8'  # weather API key
 
-
+owmAPI = OWM(W_API_key)
 T = Twython(T_API_key, T_API_key_secret,T_OAuth_TK ,T_OAuth_TK_secret)
-owm = OWM()
+
 
 def place_tweet(status):
     T.update_status(status=status)
 
 def weather():
-    testw =  owm.weather_at_place('Utrecht,NL')
-    print(testw)
-    # werkt niet
+    owm = owmAPI.weather_at_place('Utrecht,NL')
+    weather_ob = owm.get_weather()
+    clouds = weather_ob.get_clouds()  # Get cloud coverage
+    rain = weather_ob.get_rain()  # Get rain volume
+    wind = weather_ob.get_wind()  # Get wind degree and speed
+    humidity = weather_ob.get_humidity()  # Get humidity percentage
+    prs = weather_ob.get_pressure()  # Get atmospheric pressure
+    temp = weather_ob.get_temperature(unit='celsius')
+    status = weather_ob.get_detailed_status()  # Get detailed weather status
 
 def get_latest_tweets():
     timeline = T.get_user_timeline()
--- test.py
+++ test.py
@@ -1,20 +1,11 @@
-import tkinter as tk
-import time
+from twython import Twython
+from pyowm import *
 
-class App():
-    def __init__(self):
-        self.root = tk.Tk()
-        self.label = tk.Label(text="")
-        self.label.pack()
-        self.update_clock()
-        print('1')
-        self.root.mainloop()
-        print('3')
 
-    def update_clock(self):
-        print('2')
-        now = time.strftime("%H:%M:%S")
-        self.label.configure(text=now)
-        self.root.after(1000, self.update_clock)
 
-app=App()
+
+
+
+
+
+weather()
