0b5a092867e73852b0105181ce7ed2cf7760f27f
janus-magnus <Ykorringa@gmail.com>
2016-11-10 11:45:48 +0100

    v2

--- API_controler.py
+++ API_controler.py
@@ -11,14 +11,17 @@ owmAPI = OWM(W_API_key)
 T = Twython(T_API_key, T_API_key_secret,T_OAuth_TK ,T_OAuth_TK_secret)
 
 def place_tweet(status):
+    '''plaatst een tweet met de mee gegeven status als inhoud'''
     T.update_status(status=status)
 
 def weather():
+    '''haalt de weer gegevens op van openweathermap en return weather_ob'''
     owm = owmAPI.weather_at_place('Utrecht,NL')
     weather_ob = owm.get_weather()
     return weather_ob
 
 def get_latest_tweets():
+    '''haalt de timeline van twitter en return timeline'''
     timeline = T.get_user_timeline()
     return timeline
 
